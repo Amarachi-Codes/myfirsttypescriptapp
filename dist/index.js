9443be617a698d259a8fb8411e0df2d0eed55a36
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-02-16 21:24:22 +0100

    updated typescript

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let username = "Doris";
+// console.log(username);
+function getFullname(username) {
+    return username;
+}
+// console.log(getFullname("Madueke Ethel-Doris"));
+function addScores(a, b) {
+    return a + b;
+}
+// console.log(addScores(96,89));
+function stateOfOrigin(arr) {
+    for (let states of arr) {
+        //    console.log(states);
+    }
+}
+stateOfOrigin(["abia", "anambra", "imo", "delta", "lagos"]);
---
+++ index.ts
@@ -0,0 +1,26 @@
+let username:string = "Doris";
+// console.log(username);
+function getFullname(username:string){
+    return username
+}
+// console.log(getFullname("Madueke Ethel-Doris"));
+
+function addScores(a:number,b:number):number{
+    return a + b
+}
+// console.log(addScores(96,89));
+
+function stateOfOrigin(arr:string[]){
+    for(let states of arr){
+    //    console.log(states);
+       
+    }
+}
+stateOfOrigin(["abia","anambra","imo","delta","lagos"]);
+
+//Interface
+
+interface Customer{
+firstname: string;
+lastname:string;
+}
